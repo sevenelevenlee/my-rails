5467883675b4298ae9b14ce6a56bd14ee536efce
lee <yeluowoxin@sina.com>
2017-10-10 16:27:15 +0800

    use scope for search in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,15 @@ class Post < ApplicationRecord
   validates :content, :presence => true
 
   has_many :comments
+  # has_and_belongs_to_many :tags
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  scope :tag_with, lambda {|tag_name| joins(:tags).where("tags.name = ?", tag_name)}
+  scope :latter_than, lambda {|time| joins(:taggings).where("taggings.created_at > ?", time)}
+
+  #所有tag的posts
+  # def self.with_tag(tag_name)
+  #   Tag.find_by_name(tag_name).try(:posts)
+  # end
 end
---
+++ spec/factories/taggings.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :tagging do
+    post_id 1
+    tag_id ""
+  end
+end
